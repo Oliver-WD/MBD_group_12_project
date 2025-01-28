a3ffd803be10024ddacc57bf201e8ac0e8b58a85
Bohlmeijer, M.W. (Minke, Student M-CS) <m.w.bohlmeijer@student.utwente.nl>
2025-01-28 22:36:17 +0100

    Temporal data analysis code + merge parquet files

--- small_data/create_data.py
+++ small_data/create_data.py
@@ -1,49 +1,79 @@
 import pandas as pd
 import os
 
-directory = '../crash_anal'
+directory = '../crash_analysis_500m_b'
 
-data = pd.DataFrame
+data = pd.read_parquet("../crash_analysis_500m_b/year=2016/part-00070-d7900872-885f-48ba-80f2-8793c2f2b1b4.c000.snappy.parquet")
 
-for foldername in os.listdir(directory):
-    for region_folder in os.listdir(directory + '/' + foldername):
-        if '500m' in region_folder:
-            for filename in os.listdir(directory + '/' + foldername + '/' + region_folder):
-                new_data = pd.read_parquet(directory + '/' + foldername + '/' + region_folder + '/' + filename)
-                data = pd.concat([data, new_data]).groupby("crash_timestamp", as_index=False).agg({'LATITUDE':'first', 'LONGITUDE':'first', 'speed_5min_after':'mean', 'speed_15min_before':'mean', 'speed_1hr_after':'mean', 'speed_15min_after':'mean'})
+for year in os.listdir(directory):
+    for filename in os.listdir(directory + '/' + year):
+        new_data = pd.read_parquet(os.path.join(directory + '/' + year +'/' + filename))
+        data = pd.concat([data, new_data]).groupby(["crash_timestamp", "link_id"], as_index=False).agg({'LATITUDE':'first', 'LONGITUDE':'first', 'speed_5min_after':'mean', 'speed_15min_before':'mean', 'speed_1hr_after':'mean', 'speed_15min_after':'mean'})
 data = data.dropna()
-data.to_parquet('500m_data.parquet')
+data.to_parquet('500mb_data_link.parquet')
 
-data = pd.DataFrame
+# data = pd.read_parquet("../crash_anal/LINK_ID=4329472/region_size=size_500m/part-00006-eb5dbaa4-69fb-4b3c-922a-15d93c616589.c000.snappy.parquet")
+#
+# for foldername in os.listdir(directory):
+#     link_id = foldername.split('=')[1]
+#     for region_folder in os.listdir(directory + '/' + foldername):
+#         if '500m' in region_folder:
+#             for filename in os.listdir(directory + '/' + foldername + '/' + region_folder):
+#                 new_data = pd.read_parquet(directory + '/' + foldername + '/' + region_folder + '/' + filename)
+#                 new_data["LINK_ID"] = link_id
+#                 data = pd.concat([data, new_data]).groupby(["crash_timestamp", "LINK_ID"], as_index=False).agg({'LATITUDE':'first', 'LONGITUDE':'first', 'speed_5min_after':'mean', 'speed_15min_before':'mean', 'speed_1hr_after':'mean', 'speed_15min_after':'mean'})
+# data = data.dropna()
+# data.to_parquet('500m_data_link.parquet')
+#
+# data = pd.read_parquet("../crash_anal/LINK_ID=4329472/region_size=size_1km/part-00025-eb5dbaa4-69fb-4b3c-922a-15d93c616589.c000.snappy.parquet")
+#
+# for foldername in os.listdir(directory):
+#     link_id = foldername.split('=')[1]
+#     for region_folder in os.listdir(directory + '/' + foldername):
+#         if '1km' in region_folder:
+#             for filename in os.listdir(directory + '/' + foldername + '/' + region_folder):
+#                 new_data = pd.read_parquet(directory + '/' + foldername + '/' + region_folder + '/' + filename)
+#                 new_data["LINK_ID"] = link_id
+#                 data = pd.concat([data, new_data]).groupby(["crash_timestamp", "LINK_ID"], as_index=False).agg({'LATITUDE':'first', 'LONGITUDE':'first', 'speed_5min_after':'mean', 'speed_15min_before':'mean', 'speed_1hr_after':'mean', 'speed_15min_after':'mean'})
+# data = data.dropna()
+# data.to_parquet('1km_data.parquet')
+#
+# data = pd.read_parquet("../crash_anal/LINK_ID=4329472/region_size=size_2km/part-00044-eb5dbaa4-69fb-4b3c-922a-15d93c616589.c000.snappy.parquet")
+#
+# for foldername in os.listdir(directory):
+#     link_id = foldername.split('=')[1]
+#     for region_folder in os.listdir(directory + '/' + foldername):
+#         if '2km' in region_folder:
+#             for filename in os.listdir(directory + '/' + foldername + '/' + region_folder):
+#                 new_data = pd.read_parquet(directory + '/' + foldername + '/' + region_folder + '/' + filename)
+#                 new_data["LINK_ID"] = link_id
+#                 data = pd.concat([data, new_data]).groupby(["crash_timestamp", "LINK_ID"], as_index=False).agg({'LATITUDE':'first', 'LONGITUDE':'first', 'speed_5min_after':'mean', 'speed_15min_before':'mean', 'speed_1hr_after':'mean', 'speed_15min_after':'mean'})
+# data = data.dropna()
+# data.to_parquet('2km_data.parquet')
+#
+# data = pd.read_parquet("../crash_anal/LINK_ID=4329472/region_size=size_5km/part-00063-eb5dbaa4-69fb-4b3c-922a-15d93c616589.c000.snappy.parquet")
+#
+# for foldername in os.listdir(directory):
+#     link_id = foldername.split('=')[1]
+#     for region_folder in os.listdir(directory + '/' + foldername):
+#         if '5km' in region_folder:
+#             for filename in os.listdir(directory + '/' + foldername + '/' + region_folder):
+#                 new_data = pd.read_parquet(directory + '/' + foldername + '/' + region_folder + '/' + filename)
+#                 new_data["LINK_ID"] = link_id
+#                 data = pd.concat([data, new_data]).groupby(["crash_timestamp", "LINK_ID"], as_index=False).agg({'LATITUDE':'first', 'LONGITUDE':'first', 'speed_5min_after':'mean', 'speed_15min_before':'mean', 'speed_1hr_after':'mean', 'speed_15min_after':'mean'})
+# data = data.dropna()
+# data.to_parquet('5km_data.parquet')
 
-for foldername in os.listdir(directory):
-    for region_folder in os.listdir(directory + '/' + foldername):
-        if '1km' in region_folder:
-            for filename in os.listdir(directory + '/' + foldername + '/' + region_folder):
-                new_data = pd.read_parquet(directory + '/' + foldername + '/' + region_folder + '/' + filename)
-                data = pd.concat([data, new_data]).groupby("crash_timestamp", as_index=False).agg({'LATITUDE':'first', 'LONGITUDE':'first', 'speed_5min_after':'mean', 'speed_15min_before':'mean', 'speed_1hr_after':'mean', 'speed_15min_after':'mean'})
-data = data.dropna()
-data.to_parquet('1km_data.parquet')
-
-data = pd.DataFrame
-
-for foldername in os.listdir(directory):
-    for region_folder in os.listdir(directory + '/' + foldername):
-        if '2km' in region_folder:
-            for filename in os.listdir(directory + '/' + foldername + '/' + region_folder):
-                new_data = pd.read_parquet(directory + '/' + foldername + '/' + region_folder + '/' + filename)
-                data = pd.concat([data, new_data]).groupby("crash_timestamp", as_index=False).agg({'LATITUDE':'first', 'LONGITUDE':'first', 'speed_5min_after':'mean', 'speed_15min_before':'mean', 'speed_1hr_after':'mean', 'speed_15min_after':'mean'})
-data = data.dropna()
-data.to_parquet('2km_data.parquet')
-
-data = pd.DataFrame
-
-for foldername in os.listdir(directory):
-    for region_folder in os.listdir(directory + '/' + foldername):
-        if '5km' in region_folder:
-            for filename in os.listdir(directory + '/' + foldername + '/' + region_folder):
-                new_data = pd.read_parquet(directory + '/' + foldername + '/' + region_folder + '/' + filename)
-                data = pd.concat([data, new_data]).groupby("crash_timestamp", as_index=False).agg({'LATITUDE':'first', 'LONGITUDE':'first', 'speed_5min_after':'mean', 'speed_15min_before':'mean', 'speed_1hr_after':'mean', 'speed_15min_after':'mean'})
-data = data.dropna()
-data.to_parquet('5km_data.parquet')
-
+# data = pd.read_parquet("crash_average_speeds/part-00000-e8a35280-0ce9-400a-b230-c0cc478564a7-c000.snappy.parquet")
+#
+# for filename in os.listdir('crash_average_speeds'):
+#     new_data = pd.read_parquet('crash_average_speeds/' + filename)
+#     data = pd.concat([data, new_data]).groupby("DATA_AS_OF", as_index=False).agg({'avg(SPEED)':'mean'})
+# data = data.dropna()
+# data.to_parquet('average_speeds_data.parquet')
+#
+# for filename in os.listdir('average_speeds_links'):
+#     new_data = pd.read_parquet('average_speeds_links/' + filename)
+#     data = pd.concat([data, new_data]).groupby(["DATA_AS_OF", "LINK_ID"], as_index=False).agg({'avg(SPEED)':'mean'})
+# data = data.dropna()
+# data.to_parquet('average_speeds_linkdata.parquet')
--- small_data/temporal_data.py
+++ small_data/temporal_data.py
@@ -1,91 +1,171 @@
-import os
-from datetime import timedelta
-
 import pandas as pd
 import scipy.stats as stats
 from matplotlib import pyplot as plt
 from matplotlib import dates as mdates
 
+# load the crash data
+crash_data500 = pd.read_parquet('500m_data_link.parquet')
+crash_data500b = pd.read_parquet('500mb_data_link.parquet')
+crash_data1 = pd.read_parquet('1km_data.parquet')
+crash_data2 = pd.read_parquet('2km_data.parquet')
+crash_data5 = pd.read_parquet('5km_data.parquet')
+
+# choose which data to analyze
+crash_data_all = crash_data500b
+
+############# NORMALIZE THE SPEED BASED ON THE LINK_IDS, SAVED IN 5MINAVERAGE BLAH BLAH #############
 # load the average speed
-normal_data = pd.read_parquet('average_speeds.parquet')
+# normal_data_all = pd.read_parquet('average_speeds_linkdata.parquet')
+#
+# # only use pre-covid data
+# normal_data_all = normal_data_all[(normal_data_all["DATA_AS_OF"] < '2020-01-01') & (normal_data_all["DATA_AS_OF"] > '2017-01-01')]
+#
+# # ids that should be considered for calculating the average speed
+# valid_ids = pd.unique(crash_data_all["link_id"])
+#
+# crash_counts = crash_data_all["link_id"].value_counts()
+#
+# # set all dates to the same date (so that resample only considers time, not date)
+# normal_data_all["timestamp"] = pd.to_datetime(normal_data_all["DATA_AS_OF"]).dt.time
+# normal_data_all["timestamp"] = pd.to_datetime(normal_data_all["timestamp"], format="%H:%M:%S")
+# normal_data_all = normal_data_all[["timestamp", "LINK_ID", "avg(SPEED)"]]
+# normal_data_all.groupby(["timestamp", "LINK_ID"]).mean()
+# normal_data_all.set_index("timestamp", inplace=True)
+#
+# # normalize the data based on how often a link appears in the crash dataset (links appearing more often have a strong 'weight)
+# first_id = valid_ids[0]
+# normal_data_temp = normal_data_all[normal_data_all["LINK_ID"] == first_id]
+# normal_data_temp = normal_data_temp["avg(SPEED)"]
+# normal_data_temp = normal_data_temp.resample('5min').mean()
+# normal_data_temp = normal_data_temp.to_frame()
+# print(normal_data_temp)
+#
+# i = 0
+# for id in valid_ids:
+#     counts = crash_counts.loc[id]
+#     if i != 0:
+#         next_data_id = normal_data_all[normal_data_all["LINK_ID"] == id]
+#         next_data_id = next_data_id["avg(SPEED)"]
+#         next_data_id = next_data_id.resample('5min').mean()
+#         next_data_id = next_data_id.to_frame()
+#         normal_data_temp["speed"] = next_data_id["avg(SPEED)"]
+#         normal_data_temp["avg(SPEED)"] = normal_data_temp["avg(SPEED)"] * (i/(i+counts)) + normal_data_temp["speed"] * (crash_counts.loc[id]/(i+counts))
+#     i += crash_counts.loc[id]
+#
+# normal_data_temp.to_parquet('5min_average_speed_normalized.parquet')
+########################################################################################################
+
+
+# only use pre-covid data
+crash_data_all = crash_data_all[(crash_data_all["crash_timestamp"] < '2020-01-01') & (crash_data_all["crash_timestamp"] > '2017-01-01')]
 
 # set all dates to the same date (so that resample only considers time, not date)
-normal_data["timestamp"] = pd.to_datetime(normal_data["DATA_AS_OF"]).dt.time
-normal_data["timestamp"] = pd.to_datetime(normal_data["timestamp"], format="%H:%M:%S")
-normal_data.groupby(["timestamp"]).mean()
-normal_data.set_index("timestamp", inplace=True)
-
-normal_data = normal_data.resample('5min').mean()
-
-# calculate difference in average speed in the same intervals as the crash data
-normal_data["5min_diff"] = round(normal_data["avg(SPEED)"] - normal_data["avg(SPEED)"].shift(4), 2)
-normal_data["15min_diff"] = round(normal_data["avg(SPEED)"] - normal_data["avg(SPEED)"].shift(6), 2)
-normal_data["1hr_diff"] = round(normal_data["avg(SPEED)"] - normal_data["avg(SPEED)"].shift(15), 2)
-#
-# print(data)
-#
-# ax = data.plot(y=["5min_diff", "15min_diff", "1h_diff"], kind='line')
-# ax.grid(True)
-# ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
-# ax.set_ylabel('Average speed (mph)')
-#
-# plt.savefig("temporal_speeds.png")
-# plt.show()
-
-# load the crash data (500m)
-crash_data = pd.read_parquet('5km_data.parquet')
-
-crash_data["crash_timestamp"] = pd.to_datetime(crash_data["crash_timestamp"])
-crash_data.set_index(crash_data["crash_timestamp"])
-crash_data = crash_data.dropna()
-crash_data["timestamp"] = pd.to_datetime(crash_data["crash_timestamp"]).dt.time
-crash_data["timestamp"] = pd.to_datetime(crash_data["timestamp"], format="%H:%M:%S")
-crash_data.set_index("timestamp", inplace=True)
-
-
-# calculate difference in average speed before and after a crash
-crash_data["5min_diff"] = round(crash_data["speed_5min_after"] - crash_data["speed_15min_before"], 2)
-crash_data["15min_diff"] = round(crash_data["speed_15min_after"] - crash_data["speed_15min_before"], 2)
-crash_data["1hr_diff"] = round(crash_data["speed_1hr_after"] - crash_data["speed_15min_before"], 2)
+crash_data_all.set_index(crash_data_all["crash_timestamp"])
+crash_data_all = crash_data_all.dropna()
+crash_data_all["timestamp"] = pd.to_datetime(crash_data_all["crash_timestamp"]).dt.time
+crash_data_all["timestamp"] = pd.to_datetime(crash_data_all["timestamp"], format="%H:%M:%S")
+crash_data_all.set_index("timestamp", inplace=True)
 
 # drop the unnecessary columns
-crash_data = crash_data[["5min_diff", "15min_diff","1hr_diff"]]
+crash_data_all = crash_data_all[["speed_15min_before", "speed_5min_after", "speed_15min_after", "speed_1hr_after"]]
+crash_data_all["speed_15min_before"] = pd.to_numeric(crash_data_all["speed_15min_before"])
 
-# resample the data by hour and calculate the mean
+crash_data = crash_data_all
+normal_data = pd.read_parquet('5min_average_speed_normalized.parquet')
+
+print(normal_data.shape)
+print(normal_data)
+print(normal_data["avg(SPEED)"])
+print(crash_data["speed_15min_before"])
+
+# normal_data = normal_data.resample('5min').mean()
 crash_data = crash_data.resample('5min').mean()
 
-# difference between average speeds after a crash and average speeds
-data = pd.DataFrame(round(normal_data["1hr_diff"] - crash_data["1hr_diff"].shift(3), 2))
+# difference between average speeds after a crash and average speeds.
+# shifts are needed, speed_1hr_after is 01:00:00 if timestamp says 00:00:00
+data = pd.DataFrame(round(normal_data["avg(SPEED)"] - crash_data["speed_15min_before"].shift(-3), 2))
+data.columns = ['speed_diff_bef']
+data["speed_diff_5aft"] = round(normal_data["avg(SPEED)"] - crash_data["speed_5min_after"].shift(1), 2)
+data["speed_diff_15aft"] = round(normal_data["avg(SPEED)"] - crash_data["speed_15min_after"].shift(3), 2)
+data["speed_diff_1aft"] = round(normal_data["avg(SPEED)"] - crash_data["speed_1hr_after"].shift(12), 2)
 
-data = data.sort_values(by=["1hr_diff"])
+# get the difference in speed
+data["average_speed"] = normal_data["avg(SPEED)"]
+data["speed_15min_before"] = crash_data["speed_15min_before"].shift(-3)
+data["speed_5min_after"] = crash_data["speed_5min_after"].shift(1)
+data["speed_15min_after"] = crash_data["speed_15min_after"].shift(3)
+data["speed_1hr_after"] = crash_data["speed_1hr_after"].shift(12)
 data = data.dropna()
 
-# check if normally distributed
-stat, p_value = stats.shapiro(data["1hr_diff"].to_numpy())
-print(f"P-value: {p_value:.4f}")
-if p_value < 0.05:
-    print("Normally distributed")
-    # is the mean significantly different from 0?
-    t_stat, p_value = stats.ttest_1samp(data["1hr_diff"].to_numpy(), 0)
-    print(f"T-statistic: {t_stat:.4f}")
-    print(f"P-value: {p_value:.4f}")
+print(normal_data)
 
-    if p_value / 2 < 0.05:
-        if t_stat > 0:
-            print("Cars drive slower after a crash")
-        else:
-            print("Cars drive faster after a crash")
-
-
-
-ax = data["1hr_diff"].plot.hist(bins=30, alpha=0.7, edgecolor="black")
-plt.axvline(data["1hr_diff"].mean(), color="red", linestyle="dashed", linewidth=1.5, label=f"Mean: {data['1hr_diff'].mean():.2f}")
-plt.title("Distribution of 5min Differences")
-plt.xlabel("1hr_diff")
-plt.ylabel("Frequency")
-plt.legend()
+ax = data.plot(y=["average_speed"], kind='line')
+ax.grid(True)
+ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+ax.set_ylabel('Average speed (mph)')
+# plt.savefig("temporal_speeds.png")
 plt.show()
-# plt.savefig("500meter_hour_temporal.png")
-# plt.show()
 
+ax = data.plot(y=["average_speed","speed_15min_before"], kind='line')
+ax.grid(True)
+ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+ax.set_ylabel('Average speed (mph)')
+# plt.savefig("temporal_speeds.png")
+plt.show()
+
+ax = data.plot(y=["average_speed", "speed_5min_after"], kind='line')
+ax.grid(True)
+ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+ax.set_ylabel('Average speed (mph)')
+# plt.savefig("temporal_speeds.png")
+plt.show()
+
+
+ax = data.plot(y=["average_speed", "speed_15min_after"], kind='line')
+ax.grid(True)
+ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+ax.set_ylabel('Average speed (mph)')
+# plt.savefig("temporal_speeds.png")
+plt.show()
+
+ax = data.plot(y=["average_speed", "speed_1hr_after"], kind='line')
+ax.grid(True)
+ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+ax.set_ylabel('Average speed (mph)')
+# plt.savefig("temporal_speeds.png")
+plt.show()
+
+ax = data.plot(y=["average_speed", "speed_5min_after", "speed_15min_after", "speed_1hr_after"], kind='line')
+ax.grid(True)
+ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+ax.set_ylabel('Average speed (mph)')
+# plt.savefig("temporal_speeds.png")
+plt.show()
+
+if data["average_speed"].count() > 3:
+    differences = ["speed_diff_bef", "speed_diff_5aft", "speed_diff_15aft", "speed_diff_1aft"]
+    for diff in differences:
+        stat, p_value = stats.shapiro(data[diff].to_numpy())
+        print(f"P-value: {p_value:.4f}")
+        if p_value < 0.05:
+            print("Normally distributed")
+            # is the mean significantly different from 0?
+            t_stat, p_value = stats.ttest_1samp(data[diff].to_numpy(), 0)
+            print(f"T-statistic: {t_stat:.4f}")
+            print(f"P-value: {p_value:.4f}")
+
+            if p_value / 2 < 0.05:
+                ax = data[diff].plot.hist(bins=30, alpha=0.7, edgecolor="black")
+                plt.axvline(data[diff].mean(), color="red", linestyle="dashed", linewidth=1.5,
+                            label=f"Mean: {data[diff].mean():.2f}")
+                plt.title(f"Distribution of {diff}")
+                plt.xlabel(diff)
+                plt.ylabel("Frequency")
+                plt.legend()
+                plt.show()
+
+                if t_stat > 0:
+                    print("Cars drive slower after a crash")
+                else:
+                    print("Cars drive faster after a crash")
 
