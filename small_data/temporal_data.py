5d3deed872ec3dcc48ef33b5f3ead2176050731c
Bohlmeijer, M.W. (Minke, Student M-CS) <m.w.bohlmeijer@student.utwente.nl>
2025-01-29 11:38:55 +0100

    Temporal data analysis code + merge parquet files

--- small_data/temporal_data.py
+++ small_data/temporal_data.py
@@ -4,56 +4,13 @@ from matplotlib import pyplot as plt
 from matplotlib import dates as mdates
 
 # load the crash data
-crash_data500 = pd.read_parquet('500m_data_link.parquet')
 crash_data500b = pd.read_parquet('500mb_data_link.parquet')
-crash_data1 = pd.read_parquet('1km_data.parquet')
-crash_data2 = pd.read_parquet('2km_data.parquet')
-crash_data5 = pd.read_parquet('5km_data.parquet')
+crash_data1000b = pd.read_parquet('1000mb_data_link.parquet')
+crash_data2000b = pd.read_parquet('2000mb_data_link.parquet')
 
+analyzed = "500"
 # choose which data to analyze
 crash_data_all = crash_data500b
-
-############# NORMALIZE THE SPEED BASED ON THE LINK_IDS, SAVED IN 5MINAVERAGE BLAH BLAH #############
-# load the average speed
-# normal_data_all = pd.read_parquet('average_speeds_linkdata.parquet')
-#
-# # only use pre-covid data
-# normal_data_all = normal_data_all[(normal_data_all["DATA_AS_OF"] < '2020-01-01') & (normal_data_all["DATA_AS_OF"] > '2017-01-01')]
-#
-# # ids that should be considered for calculating the average speed
-# valid_ids = pd.unique(crash_data_all["link_id"])
-#
-# crash_counts = crash_data_all["link_id"].value_counts()
-#
-# # set all dates to the same date (so that resample only considers time, not date)
-# normal_data_all["timestamp"] = pd.to_datetime(normal_data_all["DATA_AS_OF"]).dt.time
-# normal_data_all["timestamp"] = pd.to_datetime(normal_data_all["timestamp"], format="%H:%M:%S")
-# normal_data_all = normal_data_all[["timestamp", "LINK_ID", "avg(SPEED)"]]
-# normal_data_all.groupby(["timestamp", "LINK_ID"]).mean()
-# normal_data_all.set_index("timestamp", inplace=True)
-#
-# # normalize the data based on how often a link appears in the crash dataset (links appearing more often have a strong 'weight)
-# first_id = valid_ids[0]
-# normal_data_temp = normal_data_all[normal_data_all["LINK_ID"] == first_id]
-# normal_data_temp = normal_data_temp["avg(SPEED)"]
-# normal_data_temp = normal_data_temp.resample('5min').mean()
-# normal_data_temp = normal_data_temp.to_frame()
-# print(normal_data_temp)
-#
-# i = 0
-# for id in valid_ids:
-#     counts = crash_counts.loc[id]
-#     if i != 0:
-#         next_data_id = normal_data_all[normal_data_all["LINK_ID"] == id]
-#         next_data_id = next_data_id["avg(SPEED)"]
-#         next_data_id = next_data_id.resample('5min').mean()
-#         next_data_id = next_data_id.to_frame()
-#         normal_data_temp["speed"] = next_data_id["avg(SPEED)"]
-#         normal_data_temp["avg(SPEED)"] = normal_data_temp["avg(SPEED)"] * (i/(i+counts)) + normal_data_temp["speed"] * (crash_counts.loc[id]/(i+counts))
-#     i += crash_counts.loc[id]
-#
-# normal_data_temp.to_parquet('5min_average_speed_normalized.parquet')
-########################################################################################################
 
 
 # only use pre-covid data
@@ -68,15 +25,32 @@ crash_data_all.set_index("timestamp", inplace=True)
 
 # drop the unnecessary columns
 crash_data_all = crash_data_all[["speed_15min_before", "speed_5min_after", "speed_15min_after", "speed_1hr_after"]]
-crash_data_all["speed_15min_before"] = pd.to_numeric(crash_data_all["speed_15min_before"])
+
+# set all dates to the same date (so that resample only considers time, not date)
+crash_data1000b.set_index(crash_data1000b["crash_timestamp"])
+crash_data1000b = crash_data1000b.dropna()
+crash_data1000b["timestamp"] = pd.to_datetime(crash_data1000b["crash_timestamp"]).dt.time
+crash_data1000b["timestamp"] = pd.to_datetime(crash_data1000b["timestamp"], format="%H:%M:%S")
+crash_data1000b.set_index("timestamp", inplace=True)
+
+# drop the unnecessary columns
+crash_data1000b = crash_data1000b[["speed_15min_before", "speed_5min_after", "speed_15min_after", "speed_1hr_after"]]
+
+crash_data1000b = crash_data1000b.resample('5min').mean()
+
+crash_data2000b.set_index(crash_data2000b["crash_timestamp"])
+crash_data2000b = crash_data2000b.dropna()
+crash_data2000b["timestamp"] = pd.to_datetime(crash_data2000b["crash_timestamp"]).dt.time
+crash_data2000b["timestamp"] = pd.to_datetime(crash_data2000b["timestamp"], format="%H:%M:%S")
+crash_data2000b.set_index("timestamp", inplace=True)
+
+# drop the unnecessary columns
+crash_data2000b = crash_data2000b[["speed_15min_before", "speed_5min_after", "speed_15min_after", "speed_1hr_after"]]
+
+crash_data2000b = crash_data2000b.resample('5min').mean()
 
 crash_data = crash_data_all
 normal_data = pd.read_parquet('5min_average_speed_normalized.parquet')
-
-print(normal_data.shape)
-print(normal_data)
-print(normal_data["avg(SPEED)"])
-print(crash_data["speed_15min_before"])
 
 # normal_data = normal_data.resample('5min').mean()
 crash_data = crash_data.resample('5min').mean()
@@ -88,6 +62,9 @@ data.columns = ['speed_diff_bef']
 data["speed_diff_5aft"] = round(normal_data["avg(SPEED)"] - crash_data["speed_5min_after"].shift(1), 2)
 data["speed_diff_15aft"] = round(normal_data["avg(SPEED)"] - crash_data["speed_15min_after"].shift(3), 2)
 data["speed_diff_1aft"] = round(normal_data["avg(SPEED)"] - crash_data["speed_1hr_after"].shift(12), 2)
+data["500vs1000"] = round(crash_data["speed_5min_after"] - crash_data1000b["speed_5min_after"], 2)
+data["500vs2000"] = round(crash_data["speed_5min_after"] - crash_data2000b["speed_5min_after"], 2)
+data["500vs100015"] = round(crash_data["speed_15min_before"] - crash_data1000b["speed_15min_before"], 2)
 
 # get the difference in speed
 data["average_speed"] = normal_data["avg(SPEED)"]
@@ -97,27 +74,27 @@ data["speed_15min_after"] = crash_data["speed_15min_after"].shift(3)
 data["speed_1hr_after"] = crash_data["speed_1hr_after"].shift(12)
 data = data.dropna()
 
-print(normal_data)
-
 ax = data.plot(y=["average_speed"], kind='line')
 ax.grid(True)
 ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
 ax.set_ylabel('Average speed (mph)')
-# plt.savefig("temporal_speeds.png")
+plt.savefig("hourly_average_speeds.png")
 plt.show()
 
 ax = data.plot(y=["average_speed","speed_15min_before"], kind='line')
 ax.grid(True)
 ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
 ax.set_ylabel('Average speed (mph)')
-# plt.savefig("temporal_speeds.png")
+ax.set_title(f'Crash speed for {analyzed} meters')
+plt.savefig("15before_speeds.png")
 plt.show()
 
 ax = data.plot(y=["average_speed", "speed_5min_after"], kind='line')
 ax.grid(True)
 ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
 ax.set_ylabel('Average speed (mph)')
-# plt.savefig("temporal_speeds.png")
+ax.set_title(f'Crash speed for {analyzed} meters')
+plt.savefig("5min_after.png")
 plt.show()
 
 
@@ -125,47 +102,47 @@ ax = data.plot(y=["average_speed", "speed_15min_after"], kind='line')
 ax.grid(True)
 ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
 ax.set_ylabel('Average speed (mph)')
-# plt.savefig("temporal_speeds.png")
+ax.set_title(f'Crash speed for {analyzed} meters')
 plt.show()
 
 ax = data.plot(y=["average_speed", "speed_1hr_after"], kind='line')
 ax.grid(True)
 ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
 ax.set_ylabel('Average speed (mph)')
-# plt.savefig("temporal_speeds.png")
+ax.set_title(f'Crash speed for {analyzed} meters')
 plt.show()
 
 ax = data.plot(y=["average_speed", "speed_5min_after", "speed_15min_after", "speed_1hr_after"], kind='line')
 ax.grid(True)
 ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
 ax.set_ylabel('Average speed (mph)')
-# plt.savefig("temporal_speeds.png")
+ax.set_title(f'Crash speed for {analyzed} meters')
 plt.show()
 
 if data["average_speed"].count() > 3:
-    differences = ["speed_diff_bef", "speed_diff_5aft", "speed_diff_15aft", "speed_diff_1aft"]
+    differences = ["speed_diff_bef", "speed_diff_5aft", "speed_diff_15aft", "speed_diff_1aft", "500vs1000", "500vs2000", "500vs100015"]
     for diff in differences:
         stat, p_value = stats.shapiro(data[diff].to_numpy())
-        print(f"P-value: {p_value:.4f}")
+        print(f"P-value: {p_value:.4f} for {diff}")
+
+        ax = data[diff].plot.hist(bins=30, alpha=0.7, edgecolor="black")
+        plt.axvline(data[diff].mean(), color="red", linestyle="dashed", linewidth=1.5,
+                    label=f"Mean: {data[diff].mean():.2f}")
+        plt.title(f"Distribution of {diff} for {analyzed} meters")
+        plt.xlabel(diff)
+        plt.ylabel("Frequency")
+        plt.legend()
+        plt.show()
+
         if p_value < 0.05:
-            print("Normally distributed")
+            print(f"{diff} for {analyzed} is normally distributed")
             # is the mean significantly different from 0?
             t_stat, p_value = stats.ttest_1samp(data[diff].to_numpy(), 0)
             print(f"T-statistic: {t_stat:.4f}")
-            print(f"P-value: {p_value:.4f}")
+            print(f"P-value: {p_value:.10f}")
 
-            if p_value / 2 < 0.05:
-                ax = data[diff].plot.hist(bins=30, alpha=0.7, edgecolor="black")
-                plt.axvline(data[diff].mean(), color="red", linestyle="dashed", linewidth=1.5,
-                            label=f"Mean: {data[diff].mean():.2f}")
-                plt.title(f"Distribution of {diff}")
-                plt.xlabel(diff)
-                plt.ylabel("Frequency")
-                plt.legend()
-                plt.show()
-
+            if p_value < 0.05:
                 if t_stat > 0:
                     print("Cars drive slower after a crash")
                 else:
                     print("Cars drive faster after a crash")
-
